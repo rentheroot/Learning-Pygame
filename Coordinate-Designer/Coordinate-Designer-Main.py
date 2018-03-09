71a0c30124947b1408cb30768623d61763a879c5
xXReneeXx <rjs2018@gmail.com>
2018-03-09 11:10:49 -0500

    started adding in coordinate drawer

--- Coordinate-Designer/Coordinate-Designer-Main.py
+++ Coordinate-Designer/Coordinate-Designer-Main.py
@@ -4,9 +4,14 @@ import pygame
 #setup main variables
 display_height = int(input("what is the width"))
 display_width = int(input("what is the height"))
+display_increment = int(input("increment of pixels"))
+
+#make rgb colors
+white = (255,255,255)
+black = (0,0,0)
 
 #set up pygame
-def pygame_setup(display_width,display_height):
+def pygame_setup(display_width,display_height,white):
 
 	#start pygame
 	pygame.init()
@@ -19,11 +24,30 @@ def pygame_setup(display_width,display_height):
 	gameDisplay.fill(white)
 
 #draw coordinate grid
-def coordinate_grid():
+def coordinate_grid(display_width,display_increment,display_height,black):
+
+	#make sure number is divisible
+	without_remainder_width = (display_width % display_increment)
+	without_remainder_width = (display_width - without_remainder_width)
+	print(without_remainder_width)
+
+	without_remainder_height = (display_height % display_increment)
+	without_remainder_height = (display_height - without_remainder_height)
+	print(without_remainder_height)
+
+	#determine number of lines to draw
+	number_of_lines_width = (without_remainder_width/display_increment)
+	numner_of_lines_height = (without_remainder_height/display_increment)
+
+	#draw necessary number of lines
+	for i in range(0, number_of_lines_width):
+		
+
+
 
 
 #main game loop
-def main_loop():
+def main_loop(display_width,display_increment,display_height, black):
 	
 	#set local variables
 
@@ -33,6 +57,8 @@ def main_loop():
 	clock = pygame.time.Clock()
 
 
+	#set up coordinate grid
+	coordinate_grid(display_width,display_increment,display_height, black)
 	#until crashed
 	while not crashed:
 
@@ -57,6 +83,6 @@ def crash_handling():
 
 
 #run functions
-pygame_setup(display_width,display_height)
-main_loop()
+pygame_setup(display_width,display_height,white)
+main_loop(display_width,display_increment,display_height, black)
 crash_handling()
